5cf85d4dad9ae3d05297f95d95bb7164779d6d8d
fzdwx <likelovec@gmail.com>
2023-08-06 09:49:02 +0800

    feat: gen timeline

--- mian.go
+++ mian.go
@@ -10,13 +10,15 @@ import (
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
 var (
-	Version = "0.0.2"
+	Version = "0.0.3"
 )
 
 func main() {
@@ -24,6 +26,7 @@ func main() {
 	cmd := root()
 	cmd.AddCommand(genFeedsCmd())
 	cmd.AddCommand(newContent())
+	cmd.AddCommand(genTimelineCmd())
 
 	perr("run", cmd.Execute())
 }
@@ -132,6 +135,151 @@ func genFeedsCmd() *cobra.Command {
 	}
 }
 
+func genTimelineCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "timeline",
+		Aliases: []string{"tl"},
+		Short:   "",
+		Run: func(cmd *cobra.Command, args []string) {
+			// 获取所有的评论
+			hasNextCommentPage := true
+			endCommentCursor := ""
+			dis := &Discussion{}
+			comments := &CommentPage{}
+			owner := os.Getenv("user")
+			name := os.Getenv("repo")
+			token := os.Getenv("token")
+			timeline := os.Getenv("timeline")
+			for hasNextCommentPage {
+				commentPage, err := getCommentPage(owner, name, token, timeline, endCommentCursor)
+				if err != nil {
+					return
+				}
+				copydata(dis, commentPage)
+
+				if 0 < commentPage.Comments.TotalCount {
+					comments.Nodes = append(comments.Nodes, commentPage.Comments.Nodes...)
+					comments.PageInfo = commentPage.Comments.PageInfo
+				}
+
+				// 是否有下一页评论
+				hasNextCommentPage = commentPage.Comments.PageInfo.HasNextPage
+				endCommentCursor = commentPage.Comments.PageInfo.EndCursor
+			}
+			dis.Comments.TotalCount = len(dis.Comments.Nodes)
+
+			_ = os.RemoveAll("./public/timeline.json")
+			f, err := os.Create("./public/timeline.json")
+			if err != nil {
+				perr("create timeline.json", err)
+				return
+			}
+
+			err = json.NewEncoder(f).Encode(dis)
+			if err != nil {
+				perr("encode timeline.json", err)
+				return
+			}
+		},
+	}
+}
+
+func copydata(dis *Discussion, page *Discussion) {
+	dis.Title = page.Title
+	dis.Body = page.Body
+	dis.BodyHTML = page.BodyHTML
+	dis.GitHubURL = page.GitHubURL
+	dis.CreatedAt = page.CreatedAt
+	dis.UpdatedAt = page.UpdatedAt
+	dis.Author = page.Author
+}
+
+func getCommentPage(owner, name, token, discussionNumber, afterCursor string) (*Discussion, error) {
+	queryFormat := `{
+		repository(owner: "%v", name: "%v") {
+			discussion(number: %v) {
+				id,
+				body,
+				bodyHTML,
+				bodyText,
+				url,
+				
+				category {
+					createdAt,
+					slug,
+					description,
+					emoji,
+					emojiHTML,
+					name,
+					updatedAt,
+				},				
+				comments(first: 100, %v) {
+					totalCount
+					nodes {
+						body
+						bodyHTML
+						createdAt
+						author {
+							avatarUrl
+							login
+							url
+						}
+						authorAssociation
+						updatedAt
+						upvoteCount
+						url
+						reactionGroups {
+							content
+							reactors(first: 1) {
+								totalCount
+							}
+						}
+					}
+					pageInfo {
+						hasNextPage
+						endCursor
+					}
+				}
+			}
+		}
+	}`
+	var result Body
+	if err := query(fmt.Sprintf(queryFormat, owner, name, discussionNumber, afterQuery(afterCursor)), token, &result); err != nil {
+		return nil, err
+	}
+	return result.Data.Repository.Discussion, nil
+}
+
+func query(body string, token string, result *Body) error {
+	req, err := http.NewRequest("POST", "https://api.github.com/graphql", strings.NewReader(queryf(body)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "bearer "+token)
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	resBodyBytes, err := ioutil.ReadAll(response.Body)
+
+	if http.StatusOK != response.StatusCode {
+		return fmt.Errorf("GraphQL query failed: %v\n%v", response.Status, string(resBodyBytes))
+	}
+
+	if err = json.Unmarshal(resBodyBytes, &result); err != nil {
+		return err
+	}
+
+	if result.Data == nil {
+		return fmt.Errorf("GraphQL query error: %v", string(resBodyBytes))
+	}
+
+	return nil
+}
+
 func genFeeds(links Links) error {
 	fp := gofeed.NewParser()
 	var feedItems []FeedItem
@@ -206,4 +354,117 @@ func perr(msg string, err error) {
 	if err != nil {
 		fmt.Println("Error:", msg, ", cause", err)
 	}
+}
+
+// Discussion is Github Discussion scheme
+type Discussion struct {
+	Number         int              `json:"number"`
+	Title          string           `json:"title"`
+	Body           string           `json:"body"`
+	BodyHTML       string           `json:"bodyHTML"`
+	Locked         bool             `json:"locked"`
+	UpvoteCount    int              `json:"upvoteCount"`
+	GitHubURL      string           `json:"url"`
+	CreatedAt      time.Time        `json:"createdAt"`
+	UpdatedAt      time.Time        `json:"updatedAt"`
+	Author         *User            `json:"author"`
+	Comments       *CommentPage     `json:"comments"`
+	ReactionGroups []*ReactionGroup `json:"reactionGroups"`
+}
+
+// CommentPage is Github Discussion Comment page scheme
+type CommentPage struct {
+	TotalCount int        `json:"totalCount"`
+	Nodes      []*Comment `json:"nodes"`
+	PageInfo   *PageInfo  `json:"pageInfo"`
+}
+
+// Comment is Github Discussion comment scheme
+type Comment struct {
+	Body              string           `json:"body"`
+	BodyHTML          string           `json:"bodyHTML"`
+	UpvoteCount       int              `json:"upvoteCount"`
+	GitHubURL         string           `json:"url"`
+	AuthorAssociation string           `json:"authorAssociation"`
+	CreatedAt         time.Time        `json:"createdAt"`
+	UpdatedAt         time.Time        `json:"updatedAt"`
+	Author            *User            `json:"author"`
+	ReactionGroups    []*ReactionGroup `json:"reactionGroups"`
+}
+
+// ReactionGroup is Github Discussion Reaction group scheme
+type ReactionGroup struct {
+	Content  string        `json:"content"`
+	Reactors *ReactionPage `json:"reactors"`
+}
+
+// ReactionPage is Github Discussion Reaction page scheme
+type ReactionPage struct {
+	TotalCount int `json:"totalCount"`
+}
+
+// PageInfo is Github GraphQL api page data info
+type PageInfo struct {
+	HasNextPage bool   `json:"hasNextPage"`
+	EndCursor   string `json:"endCursor"`
+	HasPrevPage bool   `json:"-"`
+	StartCursor string `json:"-"`
+}
+
+// Body is Github GraphQL api response body
+type Body struct {
+	Data *GithubData `json:"data"`
+}
+
+// GithubData is Github GraphQL api data
+type GithubData struct {
+	Repository *Repository `json:"repository"`
+	Viewer     *User       `json:"user"`
+}
+
+// Repository is Github repository scheme
+type Repository struct {
+	Name       string      `json:"name"`
+	URL        string      `json:"url"`
+	Discussion *Discussion `json:"discussion"`
+}
+
+// User is Github user scheme
+type User struct {
+	Login     string `json:"login"`
+	AvatarURL string `json:"avatarUrl"`
+	GitHubURL string `json:"url"`
+	Bio       string `json:"bio"`
+	Email     string `json:"email"`
+	Company   string `json:"company"`
+	Location  string `json:"location"`
+	Name      string `json:"name"`
+	Twitter   string `json:"twitterUsername"`
+}
+
+// ShowName 返回该用户的对外显示的名字
+func (u *User) ShowName() string {
+	if u.Name != "" {
+		return u.Name
+	}
+	return u.Login
+}
+
+func afterQuery(afterCursor string) string {
+	after := ""
+	if afterCursor != "" {
+		after = fmt.Sprintf(`after: "%v"`, afterCursor)
+	}
+	return after
+}
+
+// queryf 参数的值来源 https://docs.github.com/zh/graphql/overview/explorer
+func queryf(query string) string {
+	query = strings.ReplaceAll(query, "\n", "")
+	query = strings.ReplaceAll(query, "\t", " ")
+	query = strings.ReplaceAll(query, `"`, `\"`)
+	fields := strings.FieldsFunc(query, func(c rune) bool {
+		return c == ' '
+	})
+	return fmt.Sprintf(`{"query": "query %v" }`, strings.Join(fields, " "))
 }
