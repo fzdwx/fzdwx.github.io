7e0ac5e8d26cc992aa3fe481f6750e58ebe59a7c
fzdwx <likelovec@gmail.com>
2023-08-20 22:05:20 +0800

    NEW POST

--- components/content/ContentRender.vue
+++ components/content/ContentRender.vue
@@ -4,7 +4,8 @@
                         max-w-none
                         prose-pre:bg-codebg prose-pre:text-codetext prose-code:text-sm
                         prose-pre:my-0
-                        prose-hr:my-5 prose-hr:invisible
+                        prose-hr:my-5 prose-hr:border-t-just-light
+                        prose-p:my-2 prose-li:my-2 prose-ul:my-2
                         prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-base
                         prose-a:no-underline prose-a:text-just hover:prose-a:text-just-dark
                         prose-blockquote:border-l-[3px] prose-blockquote:border-l-just-lighter
