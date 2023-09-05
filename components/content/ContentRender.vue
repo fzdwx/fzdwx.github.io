7c1b9afef7f82335b23895e533df0c287711bfdf
fzdwx <likelovec@gmail.com>
2023-09-05 22:33:48 +0800

    feat: timeline add code block color

--- components/content/ContentRender.vue
+++ components/content/ContentRender.vue
@@ -9,6 +9,7 @@
                         prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5:text-lg prose-h6:text-base
                         prose-a:no-underline prose-a:text-just hover:prose-a:text-just-dark
                         prose-blockquote:border-l-[3px] prose-blockquote:border-l-just-lighter
+                        prose-code:after:content-none prose-code:before:content-none
                         "
             :value="doc"/>
 </template>
@@ -20,3 +21,15 @@ defineProps({
     }
 })
 </script>
+
+<style>
+code {
+  padding: 0.2em 0.4em;
+  margin: 0;
+  font-size: 85%;
+  white-space: break-spaces;
+  background-color: rgba(175, 184, 193, 0.2);
+  border-radius: 6px;
+}
+
+</style>
