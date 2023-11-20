2a2a0cf0772bd49ce8c2bf64dbc2b3454b90899f
like <like@minum.cloud>
2023-11-20 14:03:48 +0800

    chore: test clarity

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ useSeoMeta({
   ogDescription: config.header.description,
   ogTitle: config.header.title,
   ogType: 'website',
-  ogImageUrl:"https://gist.githubusercontent.com/fzdwx/b18ed3d40ce56468d149e63d0f58fe3d/raw/7e3ad34c650c77f65ceadef0ba096b2a25713f12/177021515-71aa6c94-7d68-4e12-a191-8d6cc73b8a83.gif"
+  ogImageUrl: "https://gist.githubusercontent.com/fzdwx/b18ed3d40ce56468d149e63d0f58fe3d/raw/7e3ad34c650c77f65ceadef0ba096b2a25713f12/177021515-71aa6c94-7d68-4e12-a191-8d6cc73b8a83.gif"
 })
 
 useHead({
@@ -50,6 +50,19 @@ useHead({
 </script>
 
 <template>
+  <script type="text/javascript">
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments)
+      };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "jtbd7f26tx");
+  </script>
+
   <Body autofocus>
   <main class="container lg:px-0 md:pb-20 md:px-8 sm:pb-16 sm:px-6 mx-auto px-7 py-10 pb-10 pt-24 l">
     <NuxtPage transition/>
